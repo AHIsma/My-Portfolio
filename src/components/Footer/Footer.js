4ef67f0fb86ad8f7486d5618c8844aaa2c70863d
Ismail AIT HADDOU <I.AITHADDOU@netia.com>
2023-05-08 21:54:00 +0200

    Update cv

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
             <Box className={classes.footer}>
                 {isMobile && <Social mobile />}
                 <Typography variant="body2" color="initial">
-                    © 2022 Ismail A. H., Inc
+                    © 2023 Ismail A. H., Inc
                 </Typography>
             </Box>
         </Container>
